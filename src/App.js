018476dbd49cea225fd59f33e4645b0a6c43a57f
abinayaarjunan <abiharsha2000@gmail.com>
2023-08-13 00:12:26 +0530

    day6

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import './App.css';
+
+// day6
+import React from 'react'
+
 
 function App() {
-   
-  const course = "half stack application development";
-  const part1 = 'fundamentals of react';
-  const exercises1 = 10;
-  const part2 = 'using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'state of a component';
-  const exercises3 = 14;
-
-
   return (
-    <div >
-      <h1>{course}</h1>
-      <ul>
-       
-        <li key={part1.toString()}>{part1}  {exercises1}</li>
-        <li key={part2.toString()} >{part2} {exercises2}</li>
-        <li key={part3.toString()} >{part3}  {exercises3}</li>
-
-      
-      
-      </ul>
-      
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
